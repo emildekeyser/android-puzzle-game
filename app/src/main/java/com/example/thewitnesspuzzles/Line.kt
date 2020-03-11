040bad6e773e2b1160b50444b870a01ba1515059
Johan Molenberghs <johan.molenberghs@student.ucll.be>
2020-03-11 14:02:45 +0100

    Line base class

---
+++ app/src/main/java/com/example/thewitnesspuzzles/Line.kt
@@ -0,0 +1,7 @@
+package com.example.thewitnesspuzzles
+
+class Line( _begin:Node, _end : Node, _obstacle :Boolean) {
+    var begin : Node = _begin
+    var end : Node = _end
+    var obstacle : Boolean = _obstacle
+}
