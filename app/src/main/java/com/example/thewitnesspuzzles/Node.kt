e7af0747858b08bc8bf6575250b7cd0486616edd
Jaron Trossaert <jaron.trossaert@student.ucll.be>
2020-03-11 14:19:58 +0100

    removed rogue semicolon in Node

--- app/src/main/java/com/example/thewitnesspuzzles/Node.kt
+++ app/src/main/java/com/example/thewitnesspuzzles/Node.kt
@@ -1,8 +1,7 @@
 package com.example.thewitnesspuzzles
 
 class Node(_xPos: Int, _yPos: Int, _dot: Boolean) {
-
-    var xPos: Int = _xPos
-    var yPos: Int = _yPos
-    var dot: Boolean = _dot;
+    val xPos: Int = _xPos
+    val yPos: Int = _yPos
+    val dot: Boolean = _dot
 }
