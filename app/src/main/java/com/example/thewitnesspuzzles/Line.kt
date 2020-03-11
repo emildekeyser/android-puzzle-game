9b6e56cb178398d06e09beacc8611bbba26eb398
Johan Molenberghs <johan.molenberghs@student.ucll.be>
2020-03-11 14:13:58 +0100

    equals and hashcode for line and node

--- app/src/main/java/com/example/thewitnesspuzzles/Line.kt
+++ app/src/main/java/com/example/thewitnesspuzzles/Line.kt
@@ -4,4 +4,33 @@ class Line( _begin:Node, _end : Node, _obstacle :Boolean) {
     var begin : Node = _begin
     var end : Node = _end
     var obstacle : Boolean = _obstacle
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Line
+
+        if (begin != other.begin) {
+            if (begin != other.end)
+                return false
+        }
+        if (end != other.end) {
+            if (end != other.begin)
+            return false
+        }
+        if (obstacle != other.obstacle) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = begin.hashCode()
+        result = 31 * result + end.hashCode()
+        result = 31 * result + obstacle.hashCode()
+        return result
+    }
+
+
 }
--- app/src/main/java/com/example/thewitnesspuzzles/Node.kt
+++ app/src/main/java/com/example/thewitnesspuzzles/Node.kt
@@ -5,4 +5,27 @@ class Node(_xPos: Int, _yPos: Int, _dot: Boolean) {
     var xPos: Int = _xPos
     var yPos: Int = _yPos
     var dot: Boolean = _dot;
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Node
+
+        if (xPos != other.xPos) return false
+        if (yPos != other.yPos) return false
+        if (dot != other.dot) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = xPos
+        result = 31 * result + yPos
+        result = 31 * result + dot.hashCode()
+        return result
+    }
+
+
 }
