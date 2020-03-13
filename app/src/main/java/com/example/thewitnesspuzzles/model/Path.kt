7cf8d94d38da2bbed8f31882ec61e846ce107604
Jaron Trossaert <jaron.trossaert@student.ucll.be>
2020-03-13 15:47:03 +0100

    beginnings of Path class v2

--- app/src/main/java/com/example/thewitnesspuzzles/model/Path.kt
+++ app/src/main/java/com/example/thewitnesspuzzles/model/Path.kt
@@ -1,7 +1,4 @@
 package com.example.thewitnesspuzzles.model
 
-import com.example.thewitnesspuzzles.model.Line
-
-class Path(_lines: Set<Line>, _yPos: Int, _dot: Boolean) {
-    var lines: Set<Line> = _lines
+class Path(var lines: Set<Line>, var currentNode: Node) {
 }
