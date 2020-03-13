83452cf35975035f77c1139c9574c8aff2151fb8
Jaron Trossaert <jaron.trossaert@student.ucll.be>
2020-03-13 16:18:54 +0100

    start of Maze logic

--- app/src/main/java/com/example/thewitnesspuzzles/model/Maze.kt
+++ app/src/main/java/com/example/thewitnesspuzzles/model/Maze.kt
@@ -1,9 +1,28 @@
 package com.example.thewitnesspuzzles.model
 
 class Maze(
-    var lines: Set<Line>,
     val id: Int,
+    var lines: Set<Line>,
     var path: Path
 ) {
 
+    fun getLinesAsList(): List<Line> {
+        return lines.toList()
+    }
+
+    fun updatePath(coordinates: Pair<Int, Int>): Boolean {
+        var coordinatesFound = false
+        for (line in lines) {
+            if (line.containsNode(coordinates)) {
+                coordinatesFound = true
+                break
+            }
+        }
+        if (!coordinatesFound) {
+            return false
+        }
+
+        // TODO continue with algorithm
+        return true
+    }
 }
