997f217d1210880018c99b98e829e982cdf3bff8
florian berden <florian.berden@student.ucll.be>
2020-03-17 00:03:14 +0100

    update menu

--- app/src/main/java/com/example/thewitnesspuzzles/MenuActivity.kt
+++ app/src/main/java/com/example/thewitnesspuzzles/MenuActivity.kt
@@ -1,8 +1,11 @@
 package com.example.thewitnesspuzzles
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.Color.red
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
@@ -22,21 +25,33 @@ class MenuActivity : AppCompatActivity() {
         val keuzebutton = Button(this)
         val closebutton = Button(this)
 
-            startbutton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            startbutton.text = "START"
-            startbutton.setOnClickListener {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            }
-            keuzebutton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            keuzebutton.text = "KIES PUZZLE"
-            keuzebutton.setOnClickListener { Toast.makeText(this@MenuActivity, "Demoversie voorziet geen extra puzzels", Toast.LENGTH_LONG).show() }
-            closebutton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-            closebutton.text = "AFSLUITEN"
-            closebutton.setOnClickListener {
-                finish();
-                System.exit(0);
-            }
+        startbutton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        startbutton.setPadding(0,1500,0,0)
+        startbutton.text = "START"
+        startbutton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        startbutton.setBackgroundColor(0x00000000)
+        startbutton.setTextColor(Color.parseColor("#ffffff"))
+        startbutton.setTextSize(TypedValue.COMPLEX_UNIT_DIP,35f)
+
+        keuzebutton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        keuzebutton.text = "KIES PUZZLE"
+        keuzebutton.setOnClickListener { Toast.makeText(this@MenuActivity, "Demoversie voorziet geen extra puzzels", Toast.LENGTH_LONG).show() }
+        keuzebutton.setBackgroundColor(0x00000000)
+        keuzebutton.setTextColor(Color.parseColor("#ffffff"))
+        keuzebutton.setTextSize(TypedValue.COMPLEX_UNIT_DIP,35f)
+
+        closebutton.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        closebutton.text = "AFSLUITEN"
+        closebutton.setOnClickListener {
+            finish();
+            System.exit(0);
+        }
+        closebutton.setBackgroundColor(0x00000000)
+        closebutton.setTextColor(Color.parseColor("#ffffff"))
+        closebutton.setTextSize(TypedValue.COMPLEX_UNIT_DIP,35f)
 
         // add Button to LinearLayout
         layout.addView(startbutton)
