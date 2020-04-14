49a07733f89923feda52d1133dae5ea56f51e151
r0705835 <thomas.nelen@student.ucll.be>
2020-04-14 17:58:57 +0200

    Refactored PuzzleConverter, functions are in order

--- app/src/main/java/com/example/thewitnesspuzzles/rendering/PuzzleConverter.kt
+++ app/src/main/java/com/example/thewitnesspuzzles/rendering/PuzzleConverter.kt
@@ -19,64 +19,34 @@ class PuzzleConverter(
 
     fun convertPuzzle(puzzleData: List<Line>):
             Pair<List<RenderableNode>, List<RenderableLine>> {
+        // Step 1
         val (nodes, lines) = unpack(puzzleData)
+        // Step 2
         absoluteUnit = calculateAbsoluteUnit(nodes)
+        // Step 3
         xMove = calculateXMove(nodes)
+        // Step 4
         yMove = calculateYMove(nodes)
+        // Step 5
         val renderableLines = lines.map { line -> convertLine(line)}
+        // Step 6
         val renderableNodes = nodes.map { node -> convertNode(node) }
         return Pair(renderableNodes, renderableLines)
     }
 
-    private fun calculateYMove(nodes: List<Node>): Float {
-        val max = (nodes.maxBy { n -> n.yPos})!!.yPos * absoluteUnit
-        val min = (nodes.minBy { n -> n.yPos})!!.yPos * absoluteUnit
-        val midMaze = (max - min) / 2
-        val midScreen = screenHeight / 2f
-        return midScreen - midMaze
+    // Step 1
+    private fun unpack(puzzleData: List<Line>): Pair<MutableList<Node>, List<Line>> {
+        val nodes = mutableListOf<Node>()
+        val lines = mutableListOf<Line>()
+        for (relLine in puzzleData){
+            lines.add(relLine)
+            nodes.add(relLine.begin)
+            nodes.add(relLine.end)
+        }
+        return Pair(nodes, lines)
     }
 
-    private fun calculateXMove(nodes: List<Node>): Float {
-        val max = (nodes.maxBy { n -> n.xPos})!!.xPos * absoluteUnit
-        val min = (nodes.minBy { n -> n.xPos})!!.xPos * absoluteUnit
-        val midMaze = (max - min) / 2
-        val midScreen = screenWidth / 2f
-        return midScreen - midMaze
-    }
-
-    private fun convertLine(line: Line): RenderableLine {
-            val (rx1, ry1) = Pair(line.begin.xPos.toFloat(), line.begin.yPos.toFloat())
-            val (rx2, ry2) = Pair(line.end.xPos.toFloat(), line.end.yPos.toFloat())
-
-            val ax1 = (rx1 * absoluteUnit) + xMove
-            val ay1 = (ry1 * absoluteUnit) + yMove
-            val ax2 = (rx2 * absoluteUnit) + xMove
-            val ay2 = (ry2 * absoluteUnit) + yMove
-
-            val left:Float
-            val top:Float
-            val right:Float
-            val bottom:Float
-            println(ry1)
-            println(ry2)
-        // TODO: this needs to be reworked to support diagonal
-        // can I remove this, use vector product ??
-        if (ry1 == ry2){ // horizontale lijn
-                left = ax1
-                top = ay1 - lineThickness / 2
-                right = ax2 //- (lineThickness * 2) // als naar end of start gaat anders volle lengte
-                bottom = ay2 + lineThickness / 2
-            } else {//if (rx1 == rx2) { // verticale lijn
-                left = ax1 - lineThickness / 2
-                top = ay1
-                right = ax2 + lineThickness / 2 // y2 == y1
-                bottom = ay2
-            }
-
-            val paint = if (line.taken) colorPalette.enabledPaint else colorPalette.disabledPaint
-            return RenderableLine(left, top, right, bottom, paint, line)
-    }
-
+    // Step 2
     private fun calculateAbsoluteUnit(nodes: List<Node>): Float {
         val maxY = (nodes.maxBy { n -> n.yPos.absoluteValue})!!.yPos.absoluteValue + 1
         val maxX = (nodes.maxBy { n -> n.xPos.absoluteValue})!!.xPos.absoluteValue + 1
@@ -85,6 +55,59 @@ class PuzzleConverter(
         return (screenWidth - space) / max
     }
 
+    // Step 3
+    private fun calculateXMove(nodes: List<Node>): Float {
+        val max = (nodes.maxBy { n -> n.xPos})!!.xPos * absoluteUnit
+        val min = (nodes.minBy { n -> n.xPos})!!.xPos * absoluteUnit
+        val midMaze = (max - min) / 2
+        val midScreen = screenWidth / 2f
+        return midScreen - midMaze
+    }
+
+    // Step 4
+    private fun calculateYMove(nodes: List<Node>): Float {
+        val max = (nodes.maxBy { n -> n.yPos})!!.yPos * absoluteUnit
+        val min = (nodes.minBy { n -> n.yPos})!!.yPos * absoluteUnit
+        val midMaze = (max - min) / 2
+        val midScreen = screenHeight / 2f
+        return midScreen - midMaze
+    }
+
+    // Step 5
+    private fun convertLine(line: Line): RenderableLine {
+        val (rx1, ry1) = Pair(line.begin.xPos.toFloat(), line.begin.yPos.toFloat())
+        val (rx2, ry2) = Pair(line.end.xPos.toFloat(), line.end.yPos.toFloat())
+
+        val ax1 = (rx1 * absoluteUnit) + xMove
+        val ay1 = (ry1 * absoluteUnit) + yMove
+        val ax2 = (rx2 * absoluteUnit) + xMove
+        val ay2 = (ry2 * absoluteUnit) + yMove
+
+        val left:Float
+        val top:Float
+        val right:Float
+        val bottom:Float
+        println(ry1)
+        println(ry2)
+        // TODO: this needs to be reworked to support diagonal
+        // can I remove this, use vector product ??
+        if (ry1 == ry2){ // horizontale lijn
+            left = ax1
+            top = ay1 - lineThickness / 2
+            right = ax2 //- (lineThickness * 2) // als naar end of start gaat anders volle lengte
+            bottom = ay2 + lineThickness / 2
+        } else {//if (rx1 == rx2) { // verticale lijn
+            left = ax1 - lineThickness / 2
+            top = ay1
+            right = ax2 + lineThickness / 2 // y2 == y1
+            bottom = ay2
+        }
+
+        val paint = if (line.taken) colorPalette.enabledPaint else colorPalette.disabledPaint
+        return RenderableLine(left, top, right, bottom, paint, line)
+    }
+
+    // Step 6
     private fun convertNode(node: Node): RenderableNode {
             val (rx, ry) = Pair(node.xPos, node.yPos)
             val ax = (rx * absoluteUnit) + xMove
@@ -104,16 +127,5 @@ class PuzzleConverter(
             }
             Circle(ax, ay, r, paint, node)
         }
-    }
-
-    private fun unpack(puzzleData: List<Line>): Pair<MutableList<Node>, List<Line>> {
-        val nodes = mutableListOf<Node>()
-        val lines = mutableListOf<Line>()
-        for (relLine in puzzleData){
-            lines.add(relLine)
-            nodes.add(relLine.begin)
-            nodes.add(relLine.end)
-        }
-        return Pair(nodes, lines)
     }
 }
